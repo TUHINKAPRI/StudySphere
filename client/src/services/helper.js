921644937c310da01e5def4b59ff4d09960d02ae
Tuhin <tuhinkapri2@gmail.com>
2024-03-28 17:32:21 +0530

    url changes

--- client/src/services/helper.js
+++ client/src/services/helper.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const baseURL = "http://localhost:4000/api/v1";
-// const baseURl="https://studysphere-3a2o.onrender.com/api/v1"
+// const baseURL = "http://localhost:4000/api/v1";
+const baseURL="https://studysphere-3a2o.onrender.com/api/v1"
 
 export const axiosInstance = axios.create({
   baseURL: baseURL,
